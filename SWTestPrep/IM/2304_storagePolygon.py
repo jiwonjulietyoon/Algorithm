8a71b2ca330b994048b6e22983a1f2f6da558754
Jiwon Juliet Yoon <45954965+jiwonjulietyoon@users.noreply.github.com>
2019-03-06 23:11:32 +0900

    Update 2304_storagePolygon.py

--- SWTestPrep/IM/2304_storagePolygon.py
+++ SWTestPrep/IM/2304_storagePolygon.py
@@ -3,49 +3,53 @@
 
 """
 
+import sys
+sys.stdin = open('../Input/tmp.txt', 'r')
+
 N = int(input())
 sticks = [list(map(int, input().split())) for _ in range(N)]
-
 sticks.sort(key=lambda x: x[0])
-print(sticks)
 
-maxh = max(sticks, key=lambda x: x[1])  # tallest stick
-maxidx = sticks.index(maxh)
-# print(maxh, maxidx)
+maxstick = max(sticks, key=lambda x: x[1])  # first encountered tallest stick
+maxidx = sticks.index(maxstick)  # idx of tallest stick, within "sticks"
+Sum = 0  # area sum
 
-area = 0
+############################ 1. Before Maximum
 
-# start ~ tallest stick
-h = sticks[0][1]  # starting height
-pos = sticks[0][0]  # x position
-for i in range(1, maxidx+1):
-    if sticks[i][1] > h: # upon meeting taller stick
+# search through sticks, from beginning until just before the first max stick
+h = sticks[0][1]  # initial height
+pos = sticks[0][0]  # initial x position
+for i in range(1, maxidx):  # start from 2nd stick
+    if sticks[i][1] > h:
         w = sticks[i][0] - pos
-        # print(w, h)
-        area += w*h
-        pos = sticks[i][0]
-        h = sticks[i][1]
+        Sum += w*h
+        h, pos = sticks[i][1], sticks[i][0]
+Sum += (maxstick[0]-pos)*h   # section right before the tallest interval
 
-# tallest stick
-# tmp = maxh[0]
-for tmp in range(N-1, maxidx-1, -1):
-    if sticks[tmp][1] == maxh:
+########################### 2. Maximum
+
+# find maximum interval: maxidx ~ maxend
+maxend = maxidx  # index of stick that marks the end of the tallest interval
+for i in range(N-1, maxidx, -1):
+    if sticks[i][1] == maxstick[1]:
+        maxend = i
         break
-area += (sticks[tmp][0]-maxidx+1)*maxh[1]
-print((sticks[tmp][0]-maxidx+1)*maxh[1])
-print(tmp)
 
-# tallest stick ~ end
-h = sticks[-1][1]
-pos = sticks[-1][0]
-for i in range(N-1, tmp-1, -1):
-    if sticks[i][1] > h:  # upon meeting taller stick
+# find area of tallest interval
+Sum += maxstick[1]*(sticks[maxend][0]-sticks[maxidx][0]+1)
+
+########################## 3. After Maximum
+
+# search through sticks, from the end until just before the last max stick
+h = sticks[-1][1]  # initial height
+pos = sticks[-1][0]  # initial x position
+for i in range(N-2, maxend, -1):
+    if sticks[i][1] > h:
         w = pos - sticks[i][0]
-        print(w, h)
-        area += w*h
-        pos = sticks[i][0]
-        h = sticks[i][1]
+        Sum += w*h
+        h, pos = sticks[i][1], sticks[i][0]
+Sum += (pos - sticks[maxend][0])*h
 
-print(area)
+print(Sum)
 
 
