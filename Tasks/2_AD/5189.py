87a20f1045f02847c907a9f5dae708797aa53a98
Jiwon <jiwonjulietyoon@gmail.com>
2019-03-20 18:44:31 +0900

    Modify AD 5189

--- Tasks/2_AD/5189.py
+++ Tasks/2_AD/5189.py
@@ -14,28 +14,9 @@
 import sys
 sys.stdin = open('../Input/5189.txt', 'r')
 
+
 def backtrack(p, c, Sum):  # current, previous
     global arr, N, Min, vis
-    if 0 not in vis:
-        Sum += arr[c-1][0]
-        if Sum < Min:
-            Min = Sum
-    if Sum > Min:
-        return
-    for i in range(2, N+1):
-        if not vis[i]:
-            c = i
-            vis[c] = 1
-            Sum += arr[p-1][c-1]
-            p = c
-            pp = p
-            backtrack(p, c, Sum)
-            p = pp
-            c = p
-            Sum -= arr[p - 1][c - 1]
-            vis[i] = 0
-
-
 
 
 
@@ -44,10 +25,62 @@ for T in range(int(input())):
     arr = [list(map(int, input().split())) for _ in range(N)]
 
     Min = 10000
-    vis = [1] + [0]*N
-    vis[1] = 1
+    vis = [0]*N
+    vis[0] = 1    # 사무실
 
-    backtrack(1, 1, 0)
+    for i in range(1, N):   # i: first destination
+        backtrack(1, i, 0)
 
     print(f"#{T+1} {Min}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#####################################################################
+
+
+# def backtrack(p, c, Sum):  # current, previous
+#     global arr, N, Min, vis
+#     if 0 not in vis:
+#         Sum += arr[c-1][0]
+#         if Sum < Min:
+#             Min = Sum
+#     if Sum > Min:
+#         return
+#     for i in range(2, N+1):
+#         if not vis[i]:
+#             c = i
+#             vis[c] = 1
+#             Sum += arr[p-1][c-1]
+#             p = c
+#             pp = p
+#             backtrack(p, c, Sum)
+#             p = pp
+#             c = p
+#             Sum -= arr[p - 1][c - 1]
+#             vis[i] = 0
+#
+#
+# for T in range(int(input())):
+#     N = int(input())
+#     arr = [list(map(int, input().split())) for _ in range(N)]
+#
+#     Min = 10000
+#     vis = [1] + [0]*N
+#     vis[1] = 1
+#
+#     backtrack(1, 1, 0)
+#
+#     print(f"#{T+1} {Min}")
 
